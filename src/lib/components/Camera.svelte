ce22517873ecd96016fdd35b28533cc728e8fde4
Guilherme Coelho <guilherme.coelho@veredaeducacao.com>
2025-01-14 18:15:51 +0000

    feat(lightposition): fix car light position

--- src/lib/components/Camera.svelte
+++ src/lib/components/Camera.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { T } from '@threlte/core';
 	import { camPos } from '$lib/applicationEnums';
+	import { OrbitControls } from '@threlte/extras';
 </script>
 
-<T.PerspectiveCamera makeDefault position={[camPos.x, camPos.y, camPos.z]} fov={30} />
+<T.PerspectiveCamera makeDefault position={[camPos.x, camPos.y, camPos.z]} fov={30}>
+	<OrbitControls enableDamping />
+</T.PerspectiveCamera>
--- src/lib/components/lights/LightPosition.svelte
+++ src/lib/components/lights/LightPosition.svelte
@@ -5,21 +5,33 @@
 	//origin  0, 1, 10
 </script>
 
-<!--> LEFT <!-->
+<!--> FRONT CAR <!-->
 
-<T.PointLight position={[camPos.x - 0.5, camPos.y - 0.5, camPos.z - 5]} intensity={30} />
+<!--> LEFT <!-->
+<T.PointLight position={[camPos.x + 2, camPos.y, camPos.z - 8]} intensity={10} />
 
 <!--> RIGHT <!-->
+<T.PointLight position={[camPos.x - 2, camPos.y, camPos.z - 8]} intensity={10} />
 
-<T.PointLight position={[camPos.x + 0.5, camPos.y - 0.5, camPos.z - 5]} intensity={30} />
+<!--> CENTER <!-->
+<T.PointLight position={[camPos.x, camPos.y + 1.5, camPos.z - 11]} intensity={30} />
+<T.PointLight position={[camPos.x, camPos.y, camPos.z - 8]} intensity={1} />
 
-<T.SpotLight
-	lookAt={[0, -5, 0]}
-	position={[camPos.x + 0.5, camPos.y + 0.5, camPos.z + 5]}
-	intensity={50}
-/>
+<!-- FRONT CAR FINAL <!-->
 
-<T.Mesh position={[camPos.x + 0.5, camPos.y + 0.5, camPos.z - 5]}>
-	<T.BoxGeometry args={[0.2, 0.2, 0.2]} />
-	<T.MeshBasicMaterial color="hotpink" />
-</T.Mesh>
+<!-- BACK CAR <!-->
+
+<!-- RIGHT <!-->
+<T.PointLight position={[camPos.x + 2, camPos.y + 0.5, camPos.z - 18]} intensity={40} />
+
+<!-- LEFT <!-->
+<T.PointLight position={[camPos.x - 2, camPos.y + 0.5, camPos.z - 18]} intensity={40} />
+
+<!-- BACK CAR FINAL <!-->
+
+<!-- SIDE CAR <!-->
+
+<T.PointLight position={[camPos.x + 4, camPos.y, camPos.z - 12]} intensity={20} />
+<T.PointLight position={[camPos.x - 4, camPos.y, camPos.z - 12]} intensity={20} />
+
+<!-- SIDE FINAL <!-->
